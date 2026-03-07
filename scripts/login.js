81365d54f269b2b93965d75ba33ce4ebda747de9
shuvro18 <shuvrobiswas404@gmail.com>
2026-03-07 14:32:46 +0600

    login page done

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,15 @@
-console.log("hello from login.js")
+document.getElementById("login-button").addEventListener("click", ()=>{
+    const username = document.getElementById("Username").value;
+    const password = document.getElementById("password").value;
+    
+    if(username !== "admin"){
+        alert("wrong username");
+        return ;
+    }else if(password !== "admin123"){
+        alert("wrong password");
+        return;
+    }else{
+        alert("login successful");
+        window.location.assign("home.html");
+    }
+})
