783fb56c278814ec95fca5f5184be61c3646a22c
shuvro18 <shuvrobiswas404@gmail.com>
2026-03-08 13:44:22 +0600

    modal created

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,14 @@
-// const allButton =document.getElementById("all-button");
-// const openButton =document.getElementById("open-button");
-// const closedButton =document.getElementById("closed-button");
+const issueContainer = document.getElementById("issue-container");
+const totalCount = document.getElementById("total-number");
+
+
+const span = (arr) => {
+  const html = arr.map(
+    (el) =>
+      `<span class="btn btn-outline ${el === "bug" ? "btn-secondary" : " btn-warning"}">${el === "bug" ? "<i class='fa-solid fa-bug'></i>" : "<i class='fa-solid fa-life-ring'></i>"} ${el}</span>`,
+  );
+  return html.join(" ");
+};
 
 function button(tab) {
   const buttons = ["all", "open", "closed"];
@@ -14,43 +22,114 @@ function button(tab) {
   }
 }
 
-const fetchFn = (f) => {
-  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+document.getElementById("all-button").addEventListener(
+  "click",
+  (ghurar = (dim) => {
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+      .then((res) => res.json())
+      .then((data) => display(data.data));
+  }),
+);
+
+
+// {
+//     "id": 2,
+//     "title": "Add dark mode support",
+//     "description": "Users are requesting a dark mode option. This would improve accessibility and user experience.",
+//     "status": "open",
+//     "labels": [
+//         "enhancement",
+//         "good first issue"
+//     ],
+//     "priority": "medium",
+//     "author": "sarah_dev",
+//     "assignee": "",
+//     "createdAt": "2024-01-14T14:20:00Z",
+//     "updatedAt": "2024-01-16T09:15:00Z"
+// }
+
+const infoFn = (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  fetch(url)
     .then((res) => res.json())
-    .then((data) => display(data.data));
+    .then((data) => showModal(data.data));
+};
+
+const showModal = (modal) => {
+  const modalBox = document.getElementById("modal-box");
+  modalBox.innerHTML = `
+  <div>
+            <h2 class="font-bold text-xl">${modal.title}</h2>
+            <div class="flex items-center gap-3 py-4">
+                <button class="btn rounded-full">${modal.status}</button>
+                <p class="text-[14px] text-neutral/50">Opened by ${modal.author}</p>
+                <p class="text-[14px] text-neutral/50">${modal.createdAt}</p>
+            </div>
+            <div class="flex gap-9 items-center py-5">
+                ${span(modal.labels)}
+            </div>
+            <p class="text-neutral/50 pb-4">${modal.description}</p>
+            <div class="bg-base-200 p-4 flex gap-39">
+                <p>Assignee: <br>
+                ${modal.author}</p>
+                <p>
+                    Priority: <br>
+                    <span class="btn rounded-full bg-pink-500">${modal.priority}</span>
+                </p>
+            </div>
+        </div>
+
+
+  `;
+  document.getElementById("my_modal_5").showModal();
 };
 
 const display = (alls) => {
+  // console.log(alls);
   const issueContainer = document.getElementById("issue-container");
   issueContainer.innerHTML = "";
-  console.log(issueContainer)
   alls.forEach((all) => {
-    const div = document.createElement("div")
-    div.innerHTML =`
+    const div = document.createElement("div");
+    div.innerHTML = `
     
-                    <div class=" border-t-3 bg-base-100 border-green-500 rounded-xl p-4 m-5">
+                    <div onclick="infoFn(${all.id})" class=" border-t-3 bg-base-100 ${all.status === "open" ? "border-green-500" : "border-violet-500"}  rounded-xl p-4 m-5">
                         <div class="flex justify-between items-center">
-                            <img src="./assets/Open-Status.png" alt="">
-                            <span class="btn rounded-full">HIGH</span>
+                            ${all.priority === "low" ? "<i class='fa-regular fa-circle-check'></i>" : "<img src='./assets/Open-Status.png'>"}
+                            <span class="btn rounded-full">${all.priority}</span>
                         </div>
                         <div>
-                            <p class="font-bold pt-2">Fix navigation menu on mobile devices</p>
-                            <p class="text-neutral/50 text-[13px]">The navigation menu doesn't collapse properly on mobile devices...</p>
+                            <p class="font-bold pt-2">${all.title}</p>
+                            <p class="text-neutral/50 text-[13px]">${all.description}</p>
                         </div>
                         <div class="flex gap-9 items-center py-5">
-                            <span><i class="fa-solid fa-bug"></i> Bug</span>
-                            <span><i class="fa-regular fa-life-ring"></i> help wanted</span>
+                            ${span(all.labels)}
                         </div>
                         <hr class="text-neutral/30">
                         <div class="text-[13px] text-neutral/50 py-3 space-y-2">
-                            <p>#1 by john_doe</p>
-                            <p>1/15/2024</p>
+                            <p>${all.author}</p>
+                            <p>${all.createdAt}</p>
                         </div>
                     </div>
     
-    `
-    issueContainer.append(div)
+    `;
+    issueContainer.append(div);
   });
 };
 
-fetchFn();
+document.getElementById("search-button").addEventListener("click", () => {
+  const input = document.getElementById("input").value.trim().toLowerCase();
+
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${input}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const apiData = data.data;
+      const filter = apiData.filter((word) =>
+        word.title.toLowerCase().includes(input),
+      );
+      display(filter);
+    });
+});
+
+window.onload = () => {
+  ghurar("dim");
+};
